b13256cd9b7952763466d52bd60d35e1a7ac8f81
radojicic23 <104728490+radojicic23@users.noreply.github.com>
2023-03-31 12:22:32 +0200

    Update test.c

--- test.c
+++ test.c
@@ -12,17 +12,17 @@ int main() {
     int j = 30;
     function(i, j);
     
-	debug(c); // c = X
+    debug(c); // c = X
     debug(str); // str = hello
     debug(i, str, function(i, j)); // i = 20 // str = hello // function(i, j) = 50
-								
-	debug_raw("counting:", 1, 2, 3, 4, 5.5); // counting: 1 2 3 4 5
-	debug_raw(str, "world!"); // hello world!
+    
+    debug_raw("counting:", 1, 2, 3, 4, 5.5); // counting: 1 2 3 4 5
+    debug_raw(str, "world!"); // hello world!
+    
+    idebug(i); // test.c:24: i = 20
+    idebug(j, str); // test.c:25: j = 30 //str = hello
 	
-	idebug(i); // test.c:24: i = 20
-	idebug(j, str); // test.c:25: j = 30 //str = hello
-								
-	idebug_raw("The return of function if:", function(i, j)); // test.c:29: The return of function if: 50
+    idebug_raw("The return of function if:", function(i, j)); // test.c:29: The return of function if: 50
 
     return 0;
 }
